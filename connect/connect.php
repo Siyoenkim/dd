30bcca7b0c785b5ed2ccd535f5b801e540f60cca
Siyoenkim <125418209+Siyoenkim@users.noreply.github.com>
2023-03-27 16:12:18 +0900

    dd

---
+++ connect/connect.php
@@ -0,0 +1,15 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pw = "root";
+    $db = "tldusrkrk";
+    $connect = new mysqli($host, $user, $pw, $db);
+    $connect -> set_charset("utf-8");
+
+    if(mysqli_connect_errno()){
+        echo "Database Connect False";
+    }else {
+        echo "Database Connect True";
+    }
+?>
+<!-- 접속 하는 법 -->
---
+++ sample/sample04.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        a {
+            text-decoration: none;
+            color: #000;
+        }
+        main {
+            display: flex;
+            justify-content: space-between;
+        }
+        aside {
+            width: 30%;
+            background-color: #F5F5F5;
+        }
+        section {
+            width: 70%;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <aside>
+            <ul>
+                <li><a href="#">전체 회원 목록</a></li>
+                <li><a href="#">전체 회원 목록</a></li>
+                <li><a href="#">전체 회원 목록</a></li>
+                <li><a href="#">전체 회원 목록</a></li>
+                <li><a href="#">전체 회원 목록</a></li>
+            </ul>
+        </aside>
+        <section>section</section>
+    </main>
+</body>
+</html>
